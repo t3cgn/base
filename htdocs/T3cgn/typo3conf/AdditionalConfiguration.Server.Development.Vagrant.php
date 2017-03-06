36ade823f6c809174bfe4aa731c96bea72bb39a6
Philipp Thiele <philipp.thiele@phth.de>
2017-03-06 20:50:11 +0100

    [TASK] adapted AdditionalConfiguration for TYPO3 8.6

--- htdocs/T3cgn/typo3conf/AdditionalConfiguration.Server.Development.Vagrant.php
+++ htdocs/T3cgn/typo3conf/AdditionalConfiguration.Server.Development.Vagrant.php
@@ -1,9 +1,11 @@
 <?php
 
-$GLOBALS['TYPO3_CONF_VARS']['DB']['host'] = 'localhost';
-$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'vagrant';
-$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 'vagrant';
-$GLOBALS['TYPO3_CONF_VARS']['DB']['password'] = 'vagrant';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] = 'localhost';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = 'vagrant';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = 'vagrant';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] = 'vagrant';
 $GLOBALS['TYPO3_CONF_VARS']['BE']['installToolPassword'] = '$P$CfQEMnMs/X08N4vG7Cqgyb/aMdRvB91';
+$GLOBALS['TYPO3_CONF_VARS']['BE']['compressionLevel'] = '0';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['compressionLevel'] = '0';
 $GLOBALS['TYPO3_CONF_VARS']['GFX']['im_version_5'] = 'im6';
 
